c0a0d48587278811357aae00fd9e1414bd6c1306
skat00sh <devendra.y12@lnmiit.ac.in>
2020-09-28 21:59:42 +0200

    Making it a bit more modular

--- src/find_boundary.py
+++ src/find_boundary.py
@@ -23,21 +23,29 @@ output_img = utils.convert_img_to_binary(output_img, threshold=200)
 
 
 # Assignment 1 : Find surface borders Using numpy and Python only
-kernel = np.ones((3, 3), np.uint8)
-ero = utils.morph_operation(input_img, "erosion", kernel).astype(int) # Eroded Image
+def find_boundary():
+    kernel = np.ones((3, 3), np.uint8)
+    ero = utils.morph_operation(input_img, "erosion", kernel).astype(int) # Eroded Image
 
-dil = utils.morph_operation(input_img, "dilation", kernel).astype(int) #Dilated Image
+    dil = utils.morph_operation(input_img, "dilation", kernel).astype(int) #Dilated Image
 
-diff = dil - ero
-plt.imshow(diff, cmap='Greys', interpolation='nearest')
-plt.show()
+    diff = dil - ero
+    plt.imshow(diff, cmap='Greys', interpolation='nearest')
+    plt.show()
+    return diff
 
 # Assignment 2 : List of all connected components in the image with boundary
-labeled,comp = label(diff,kernel) #label all connected components from 1,2,....n
+def create_list_of_boundaries(diff, kernel):
+    labeled,comp = label(diff,kernel) #label all connected components from 1,2,....n
 
-h,w = diff.shape
-indices = np.indices((w,h)).T[:,:,[0,1]] #create a array of indices of a grid
-buildings={}
-for i in range(1,comp+1):
-    # print(indices[labeled==i])
-    buildings[i] = indices[labeled == i]
+    h,w = diff.shape
+    indices = np.indices((w,h)).T[:,:,[0,1]] #create a array of indices of a grid
+    buildings={}
+    for i in range(1,comp+1):
+        # print(indices[labeled==i])
+        buildings[i] = indices[labeled == i]
+
+if __name__ == '__main__':
+    diff = find_boundary()
+    kernel = np.ones((3, 3), np.uint8)
+    create_list_of_boundaries(diff,kernel)
