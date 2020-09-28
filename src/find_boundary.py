0337cb9f03b9febe0d9e87615300668d6639cfd8
skat00sh <devendra.y12@lnmiit.ac.in>
2020-09-28 19:26:35 +0200

    Test: Scipy Morphological Gradient

--- src/find_boundary.py
+++ src/find_boundary.py
@@ -1,8 +1,9 @@
 import numpy as np
 import os
 from PIL import Image
-
-
+import utils
+from scipy.ndimage import binary_erosion, binary_dilation
+import matplotlib.pyplot as plt
 
 PROJECT_ROOT = utils.get_project_root()
 DATA_DIR = os.path.join(PROJECT_ROOT, "input")
@@ -17,3 +18,16 @@ output_img = Image.open(output_img_path)
 input_img = utils.convert_img_to_binary(input_img, threshold=200)
 
 output_img = utils.convert_img_to_binary(output_img, threshold=200)
+
+## Explore existing options to do the task
+kernel = np.ones((3, 3), np.uint8) #To allow every type of connection
+
+eroded_img = binary_erosion(input_img, structure=kernel).astype(int)
+plt.imshow(eroded_img,cmap='Greys',  interpolation='nearest')
+
+dilated_img = binary_dilation(input_img, structure=kernel).astype(int)
+plt.imshow(dilated_img,cmap='Greys',  interpolation='nearest')
+
+diff = dilated_img - eroded_img
+plt.imshow(diff,cmap='Greys',  interpolation='nearest')
+plt.show()
