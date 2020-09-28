7441d28c87ab0a44b65e26ef6e59ac83b6e1281a
skat00sh <devendra.y12@lnmiit.ac.in>
2020-09-28 21:39:40 +0200

    Working: List of all boundaries

--- src/find_boundary.py
+++ src/find_boundary.py
@@ -3,6 +3,8 @@ import os
 from PIL import Image
 import utils
 from scipy.ndimage import binary_erosion, binary_dilation
+from scipy.ndimage.measurements import label
+
 import matplotlib.pyplot as plt
 
 PROJECT_ROOT = utils.get_project_root()
@@ -32,7 +34,7 @@ output_img = utils.convert_img_to_binary(output_img, threshold=200)
 # plt.imshow(diff,cmap='Greys',  interpolation='nearest')
 # plt.show()
 
-
+# Assignment 1 : Find surface borders Using numpy and Python only
 kernel = np.ones((3, 3), np.uint8)
 ero = utils.morph_operation(input_img, "erosion", kernel).astype(int)
 # plt.imshow(ero, cmap='Greys', interpolation='nearest')
@@ -41,8 +43,15 @@ dil = utils.morph_operation(input_img, "dilation", kernel).astype(int)
 # plt.imshow(dil, cmap='Greys', interpolation='nearest')
 # plt.show()
 diff = dil - ero
-
 plt.imshow(diff, cmap='Greys', interpolation='nearest')
 plt.show()
 
+# Assignment 2 : List of all connected components in the image with boundary
+labeled,comp = label(diff,kernel) #label all connected components from 1,2,....n
 
+h,w = diff.shape
+indices = np.indices((w,h)).T[:,:,[0,1]] #create a array of indices of a grid
+buildings={}
+for i in range(1,comp+1):
+    # print(indices[labeled==i])
+    buildings[i] = indices[labeled == i]
